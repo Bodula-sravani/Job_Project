dbe02c8dedbf6a596c964a430e716f54e3188229
Bodula-sravani <sravanibodula@gmail.com>
2023-04-12 17:03:28 +0530

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,85 @@
+﻿using Job_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Job_Project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public List<Jobs> jobsList;
+        IConfiguration configuration;
+        public AdminController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public IActionResult CreateJob()
+            {
+                jobsList = new List<Jobs>();
+                try
+                {
+                    string connString = configuration.GetConnectionString("jobsDB");
+                    SqlConnection conn = new SqlConnection(connString);
+                    conn.Open();
+                    SqlCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = "select * from jobs";
+                    var reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Jobs job = new Jobs();
+
+                        job.Id = (int)reader["Job_id"];
+                        job.Name = (string)reader["Job_name"];
+                        job.employeerName = (string)reader["job_employeer_name"];
+                        job.category = (string)reader["job_category"];
+                        job.Description = (string)reader["job_Description"];
+
+                        jobsList.Add(job);
+
+
+                    }
+                    reader.Close();
+
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                ViewData["jobsList"] = jobsList;
+                
+                return View();
+            }
+
+
+        public IActionResult UpdateJob(int id)
+        {
+            Console.WriteLine("update method id: "+id);
+
+            string connString = configuration.GetConnectionString("jobsDB");
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = $"select * from jobs where job_id = {id}";
+            var reader = cmd.ExecuteReader();
+            Jobs job = new Jobs();
+            while (reader.Read())
+            {
+
+                job.Id = (int)reader["Job_id"];
+                job.Name = (string)reader["Job_name"];
+                job.employeerName = (string)reader["job_employeer_name"];
+                job.category = (string)reader["job_category"];
+                job.Description = (string)reader["job_Description"];
+            }
+            ViewData["job"] = job;
+            return View();
+        }
+
+        public IActionResult DeleteJob()
+        {
+            return View();
+        }
+        }
+    }
+
